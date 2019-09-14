946aa045b16854e8a080b9fead4c20c8e01de48e
TechAle <alessandro.condello.email@gmail.com>
2019-09-14 14:20:28 +0200

    “commit”

---
+++ upload.sh
@@ -0,0 +1,8 @@
+echo "# SchoolExcercises-4bin-" >> README.md
+git init
+git add Informatica
+git add TecnologiaProgettazione
+git commit -m “commit”
+git remote add origin https://github.com/TechAle/SchoolExcercises-4bin-.git
+git pull origin master
+git push -u origin master
