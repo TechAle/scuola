41a51f11efeb2d698d5b74a60f7a1d4df777dc58
TechAle <54703079+TechAle@users.noreply.github.com>
2019-11-11 09:06:34 +0100

    Update 002_Media.c

--- Informatica/005_EserciziAllocazioneDinamica/000_LaboratorioQuarte/002_Media.c
+++ Informatica/005_EserciziAllocazioneDinamica/000_LaboratorioQuarte/002_Media.c
@@ -6,15 +6,47 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-void media(int val, int len_)
+void media(int val[], int len_)
 {
-	int *vet;
-	vet
+	int *vet,
+		i,
+		j,
+		cont = 1;
+	float media = 0;
+	vet = (int*)malloc(len_*sizeof(int));
+	vet = val;
+	for(i = 1 ; i < len_ ; i++)
+	{
+		j = 0;
+		do
+		{
+			if ( *(vet + cont - 1 + j) != *(val + i ) )
+			{
+				j++;
+			}
+			else
+			{
+				j = cont+1;
+			}
+		}while(j < cont);
+		if (j != cont+1)
+		{
+			*(vet + cont) = *(val+i);
+			cont++;
+		}
+	}
+	// Somma
+	for(i = 0 ; i < cont ; i++)
+		media += *(vet+i);
+	printf("%f",media/cont);
+	
+	
+
 }
 int main()
 {
 	int len_ = 5;
 	int val[5] = {5,1,5,9,1};
 	
-	media(val,len_)
+	media(val,len_);
 }
