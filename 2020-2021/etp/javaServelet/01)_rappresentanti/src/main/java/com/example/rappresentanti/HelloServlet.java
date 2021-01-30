a5c7817d56d1a8a87ed494206b5b9cdfe9d32030
TechAle <alessandro.condello.email@gmail.com>
2021-01-30 22:48:34 +0100

    Compiti + Nuovo compito

--- 2020-2021/etp/javaServelet/01)_rappresentanti/src/main/java/com/example/rappresentanti/HelloServlet.java
+++ 2020-2021/etp/javaServelet/01)_rappresentanti/src/main/java/com/example/rappresentanti/HelloServlet.java
@@ -8,24 +8,24 @@ import javax.servlet.annotation.*;
 
 @WebServlet(name = "classe", value = "/classe")
 public class HelloServlet extends HttpServlet {
-    private String message;
+    int si, no;
 
     private final HashMap<String, String[]> classi = new HashMap<String, String[]>() {
         {
-            put("5ain", new String[] {"Alessandro Mattia", "Luca Valerio"});
+            put("5ai", new String[] {"Alessandro Mattia", "Luca Valerio"});
             put("5ac", new String[] {"Giorgio Valeotti"});
-            put("5bin", new String[] {"Giorgio Rosa", "Tiziano Rossi"});
+            put("5bi", new String[] {"Giorgio Rosa", "Tiziano Rossi"});
             put("5bc", new String[] {"Enrico Elio", "Federica Beracchia", "Marco Ferno"});
         }
     };
 
-    String primaParte = "<!DOCTYPE html>\n<html lang=\"en\" style=\"height: 100vh\">\n<head>\n    <meta charset=\"UTF-8\">\n    <title>Fauser</title>\n    <link rel=\"stylesheet\" href=\"css/bootstrap.css\">\n    <script type=\"text/javascript\" src=\"js/bootstrap.js\"></script>\n    <style>\n        label {\n            padding: 0px 1px 0px 8px;\n        }\n\n        input[type=radio],\n        input.radio {\n            margin: 2px 0 0 2px;\n        }\n         footer {\n             position: fixed;\n             left: 0;\n             bottom: 0;\n             width: 100%;\n             text-align: center;\n         }\n\n    </style>\n</head>\n<body style=\"height: 100%\">\n\n<nav class=\"navbar navbar-light bg-dark\">\n    <span class=\"navbar-brand mb-0 h1\" style=\"color: #ffc11f\">Indietro</span>\n    <span class=\"navbar-brand mb-0 h1\" style=\"color: #ffc11f\">Fauser</span>\n</nav>\n\n<div class=\"bg-light\" style=\"margin-bottom: 20px\">\n    <center>\n    <div class=\"navbar-brand mb-0 h1\" style=\"color: #1fddff; \">Rappresentanti </div>\n    </center>\n    <div class=\"navbar-brand mb-0 h5\" style=\"color: #1fddff; padding-left: 10px\">Classe ";
+    String primaParte = "<!DOCTYPE html>\n<html lang=\"en\" style=\"height: 100vh\">\n<head>\n    <meta charset=\"UTF-8\">\n    <title>Fauser</title>\n    <link rel=\"stylesheet\" href=\"css/bootstrap.css\">\n    <script type=\"text/javascript\" src=\"js/bootstrap.js\"></script>\n    <style>\n        label {\n            padding: 0px 1px 0px 8px;\n        }\n\n        input[type=radio],\n        input.radio {\n            margin: 2px 0 0 2px;\n        }\n         footer {\n             position: fixed;\n             left: 0;\n             bottom: 0;\n             width: 100%;\n             text-align: center;\n         }\n\n    </style>\n</head>\n<body style=\"height: 100%\">\n\n<nav class=\"navbar navbar-light bg-dark\">\n    <span class=\"navbar-brand mb-0 h1\" style=\"color: #ffc11f\">Indietro</span>\n    <span class=\"navbar-brand mb-0 h1\" style=\"color: #ffc11f\">Fauser</span>\n</nav>\n\n<div class=\"bg-light\" style=\"margin-bottom: 20px\">\n    <center>\n    <div class=\"navbar-brand mb-0 h1\" style=\"color: #1fddff; \">Rappresentanti </div>\n    </center>\n    <div class=\"navbar-brand mb-0 h5\" style=\"color: #1fddff; padding-left: 10px\">";
     String secondaParte = "</div>\n</div>\n\n<ul class=\"list-group\">";
     String terzaParte = "</ul>\n\n\n<footer class=\"bg-light text-center text-lg-start\">\n    <!-- Copyright -->\n    <div class=\"text-center p-3\" style=\"background-color: rgba(0, 0, 0, 0.2)\">\n        © 2020 Copyright: Fauser\n    </div>\n    <!-- Copyright -->\n</footer>\n\n</body>\n</html>";
 
 
     public void init() {
-
+        si = no = 0;
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -33,11 +33,12 @@ public class HelloServlet extends HttpServlet {
 
         String classe = request.getParameter("classe");
 
-        if (classe != null) {
+        if (classe != null && !classe.equals("")) {
             classe = escapeHTML(classe.toLowerCase());
             if (classi.containsKey(classe)) {
                 PrintWriter out = response.getWriter();
                 out.println(primaParte);
+                out.println("Classe ");
                 out.println(classe);
                 out.println(secondaParte);
                 for(String rappresentante : classi.get(classe)) {
@@ -46,7 +47,7 @@ public class HelloServlet extends HttpServlet {
                     out.println("</li>");
                 }
                 out.println(terzaParte);
-            }else response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            }else response.setStatus(HttpServletResponse.SC_NOT_FOUND);
         }else response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 
     }
@@ -64,6 +65,30 @@ public class HelloServlet extends HttpServlet {
         return sb.toString();
     }
 
-    public void destroy() {
+    protected void doPost(javax.servlet.http.HttpServletRequest request,
+                          javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException,
+            IOException {
+
+        String siVal = request.getParameter("si");
+
+        if (siVal == null)
+            no++;
+        else
+            si++;
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println(primaParte);
+        out.println("Grazie per avere votato!");
+        out.println(secondaParte);
+        out.println("<li class=\"list-group-item\">");
+        out.println("Voti si: " + si);
+        out.println("</li>");
+        out.println("<li class=\"list-group-item\">");
+        out.println("Voti no: " + no);
+        out.println("</li>");
+        out.println(terzaParte);
+    }
+
+        public void destroy() {
     }
 }
--- 2020-2021/etp/javaServelet/01)_rappresentanti/target/rappresentanti-1.0-SNAPSHOT/index.jsp
+++ 2020-2021/etp/javaServelet/01)_rappresentanti/target/rappresentanti-1.0-SNAPSHOT/index.jsp
@@ -32,7 +32,7 @@
     <span class="navbar-brand mb-0 h1" style="color: #ffc11f">Fauser</span>
 </nav>
 
-<div class="bg-light" style="margin-bottom: 20px">
+<div  class="bg-light" style="margin-bottom: 20px">
     <center>
         <div class="navbar-brand mb-0 h1" style="color: #1fddff; ">Rappresentanti</div>
     </center>
@@ -42,9 +42,9 @@
 <form action="classe" method="get">
     <div class="row" style="text-align: center; width: 100%">
 
-        <div class="col col-12 col-md-6"><label for="5AIN">5AIN</label><input type="radio" id="5AIN" name="classe" value="5AIN"></div>
+        <div class="col col-12 col-md-6"><label for="5AI">5AI</label><input type="radio" id="5AI" name="classe" value="5AI"></div>
         <div class="col col-12 col-md-6"><label for="5AC">5AC</label><input type="radio" id="5AC" name="classe" value="5AC"></div>
-        <div class="col col-12 col-md-6"><label for="5BIN">5BIN</label><input type="radio" id="5BIN" name="classe" value="5BIN"></div>
+        <div class="col col-12 col-md-6"><label for="5BI">5BI</label><input type="radio" id="5BI" name="classe" value="5BI"></div>
         <div class="col col-12 col-md-6"><label for="5BC">5BC</label><input type="radio" id="5BC" name="classe" value="5BC"></div>
         <div class="col col-12"><button type="submit" class="btn btn-outline-primary">invia</button></div>
     </div>
@@ -52,9 +52,19 @@
 
 
 <div class="row" style="text-align: center; width: 100%">
-    <div class="col col-12 col-md-6">Valutaci!</div>
-    <div class="col col-12 col-md-6">Contatta la 5bin!</div>
+    <div class="col col-12 col-md-6" id="valutaci"><a>Valutaci!</a></div>
+    <div class="col col-12 col-md-6"><a href="classe?classe=5bi">Contatta la 5bi!</a></div>
 </div>
+<form action="classe" method="post" style="margin-top: 15px; visibility: hidden" id="votazione">
+    <div class="row" style="text-align: center">
+    <div  class="col col-12 col-md-6">
+    <button type="submit" class="btn btn-success" name="si" value="mi piace">si</button>
+    </div>
+    <div  class="col col-12 col-md-6">
+        <button type="submit" class="btn btn-danger" name="no" value="non mi piace">no</button>
+    </div>
+    </div>
+</form>
 
 
 
@@ -65,6 +75,15 @@
     </div>
     <!-- Copyright -->
 </footer>
+
+<script>
+    window.addEventListener('click', function(e){
+        if (document.getElementById('valutaci').contains(e.target)){
+            document.getElementById("votazione").style.visibility = "visible";
+        } else{
+        }
+    })
+</script>
 
 </body>
 </html>
