d00ddd443693b32cfc56afb25e53b13c3c41aa1a
TechAle <alessandro.condello.email@gmail.com>
2021-03-20 16:44:38 +0100

    base html del nuovo compito (uff)

---
+++ 2020-2021/informatica/marzo/scuola/base license/sidebar/README.markdown
@@ -0,0 +1,5 @@
+# Sidebar
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/deryfwhiletrue/pen/YxNNeK](https://codepen.io/deryfwhiletrue/pen/YxNNeK).
+
+Mockup for a project
---
+++ 2020-2021/informatica/marzo/scuola/base license/sidebar/dist/script.js
@@ -0,0 +1,34 @@
+$(".small").click(function(){
+  if($(".logo").css("left") == "0px") {
+    $(".logo").css("left","-214px");
+    $(".sidebar").css("left","-214px");
+    $("#page-content").css("left","56px");
+    $(".small").text("»");
+  } else {
+    $(".logo").css("left","0");
+    $(".sidebar").css("left","0");
+    $("#page-content").css("left","270px");
+    $(".small").text("«");
+  }
+})
+$("#left li").hover(function(){
+  $("#left li").removeClass("hover")
+  $(this).addClass("hover")
+},function(){
+  $(this).removeClass("hover")
+})
+$("#left li").click(function(){
+  $("#left li").removeClass("active");
+  $(this).addClass("active");
+})
+if($(window).width() <= 360) {
+  $(".logo").css("left","-214px");
+  $(".sidebar").css("left","-214px");
+  $(".small").text("»");
+  $("#profilepic").css("margin-right","10px");
+  $("#name i").hide();
+  $("#name").css("color","white");
+  $("#name span").css("color","black");
+  $("#name").css("width","calc(100% - 55px)")
+  $("#home-nav li").css({"float":0,"width":"100%"})
+}
